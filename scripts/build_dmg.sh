708825ed1b01d8a9009f192046f942d6acd3afc7
RussReign <3178906285@qq.com>
2026-06-30 15:23:38 +0800

    chore: bump DMG package version to 1.6

--- scripts/build_dmg.sh
+++ scripts/build_dmg.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # ConferenceDeadline macOS .dmg packager
 # Usage: ./scripts/build_dmg.sh
 
-VERSION="1.5"
+VERSION="1.6"
 APP_NAME="ConferenceDeadline"
 BUNDLE_ID="com.bxvalor.conference-deadline"
 
